96a1347ef0da61db2e9fcc887debd15b9709ae19
eyalewin <eyalewin@gmail.com>
2016-05-28 16:44:51 +0000

    Add files via upload

--- home.js
+++ home.js
@@ -146,6 +146,41 @@ function buildTable(result) {
         $('.tableContent').append(st);
     }
 
+    $('.tableHeader').click(function () {
+        var header = $(this);
+        var column = header.data('index');
+        var $table = header.parents('.tableContainer');
+        var $rows = $table.find('.tableContent .tableRow:visible');
+        var type;
+        if (header.data('type') != undefined) {
+            if (header.data('type') === 'desc') {
+                header.data('type', 'asc');
+            } else {
+                header.data('type', 'desc');
+            }
+        } else {
+            header.data('type', 'asc');
+        }
+
+        type = header.data('type');
+
+        $('.tableRow').sortElements(function (a, b) {
+            if (type === 'desc') {
+                if (column === 0) {
+                    return parseInt($(a).find('span').eq(column).text()) < parseInt($(b).find('span').eq(column).text()) ? 1 : -1;
+                } else {
+                    return $(a).find('span').eq(column).text().toLowerCase() < $(b).find('span').eq(column).text().toLowerCase() ? 1 : -1;
+                }
+            } else {
+                if (column === 0) {
+                    return parseInt($(a).find('span').eq(column).text()) > parseInt($(b).find('span').eq(column).text()) ? 1 : -1;
+                } else {
+                    return $(a).find('span').eq(column).text().toLowerCase() > $(b).find('span').eq(column).text().toLowerCase() ? 1 : -1;
+                }
+            }
+        });
+    });
+
     $('.tableFotter input').keyup(function (e) {
         /* Ignore tab key */
         var code = e.keyCode || e.which;
@@ -158,50 +193,56 @@ function buildTable(result) {
         var $rows = $table.find('.tableContent .tableRow');
 
         //var $rows = $table.find('.tableContent .tableRow:visible');
-        //if (code == '8') {
-        //    $rows = $table.find('.tableContent .tableRow');
-        //}
+        if (code == '8') {
+            $rows = $table.find('.tableContent .tableRow');
+        } else if (rows != undefined) {
+            $rows = rows;
+        }
 
         debugger;
-        var inputLength = $input.parents('.tableFotter').find('input').length;
-        var inputDataCount = 0;
-        var arrayInput = [];
-        for (var i = 0; i < inputLength; i++) {
-            var item = $input.parents('.tableFotter').find('input')[i];
-            if ($(item).val() != "") {
-                inputDataCount++;
-                arrayInput.push($(item).data('index'));
-            }
-        }
+        //var inputLength = $input.parents('.tableFotter').find('input').length;
+        //var inputDataCount = 0;
+        //var arrayInput = [];
+        //for (var i = 0; i < inputLength; i++) {
+        //    var item = $input.parents('.tableFotter').find('input')[i];
+        //    if ($(item).val() != "") {
+        //        inputDataCount++;
+        //        arrayInput.push($(item).data('index'));
+        //    }
+        //}
 
         var $filteredRows = $rows.filter(function () {
             var value = $(this).find('span').eq(column).text().toLowerCase();
             return value.indexOf(inputContent) === -1;
         });
 
-        if (inputDataCount > 1) {
-            $rows.show();
-            $filteredRows.hide();
+        //if (inputDataCount > 1) {
+        //    $rows.show();
+        //    $filteredRows.hide();
 
-            for (var i = 0; i < arrayInput.length; i++) {
-                inputContent = $($input.parents('.tableFotter').find('input')[arrayInput[i]]).val().toLowerCase();
-                var $secondfilteredRows = $table.find('.tableContent .tableRow:visible').filter(function () {
-                    var value = $(this).find('span').eq(arrayInput[i]).text().toLowerCase();
-                    return value.indexOf(inputContent) === -1;
-                });
+        //    for (var i = 0; i < arrayInput.length; i++) {
+        //        inputContent = $($input.parents('.tableFotter').find('input')[arrayInput[i]]).val().toLowerCase();
+        //        var $secondfilteredRows = $table.find('.tableContent .tableRow:visible').filter(function () {
+        //            var value = $(this).find('span').eq(arrayInput[i]).text().toLowerCase();
+        //            return value.indexOf(inputContent) === -1;
+        //        });
 
-                $filteredRows = $secondfilteredRows;
-            }
+        //        $filteredRows = $secondfilteredRows;
+        //    }
 
-            $secondfilteredRows.hide();
-        } else {
-            $rows.show();
-            $filteredRows.hide();
-        }
+        //    $secondfilteredRows.hide();
+        //} else {
+        //    $rows.show();
+        //    $filteredRows.hide();
+        //}
 
+        $rows.show();
+        $filteredRows.hide();
+
+        rows = $table.find('.tableContent .tableRow:visible');
 
         if ($filteredRows.length === $rows.length) {
-            //$table.find('tableContent').prepend($('<div class="no-result text-center"><td colspan="' + $table.find('.filters th').length + '">No result found</td></tr>'));
+            $table.find('.tableContent').prepend($('<div class="no-result text-center">No result found</div>'));
         }
     });
 }
@@ -210,3 +251,49 @@ $(function () {
     //WCFJSON();
     getPersons();
 });
+
+jQuery.fn.sortElements = (function () {
+
+    var sort = [].sort;
+
+    return function (comparator, getSortable) {
+
+        getSortable = getSortable || function () { return this; };
+
+        var placements = this.map(function () {
+
+            var sortElement = getSortable.call(this),
+                parentNode = sortElement.parentNode,
+
+                // Since the element itself will change position, we have
+                // to have some way of storing its original position in
+                // the DOM. The easiest way is to have a 'flag' node:
+                nextSibling = parentNode.insertBefore(
+                    document.createTextNode(''),
+                    sortElement.nextSibling
+                );
+
+            return function () {
+
+                if (parentNode === this) {
+                    throw new Error(
+                        "You can't sort elements if any one is a descendant of another."
+                    );
+                }
+
+                // Insert before flag:
+                parentNode.insertBefore(this, nextSibling);
+                // Remove flag:
+                parentNode.removeChild(nextSibling);
+
+            };
+
+        });
+
+        return sort.call(this, comparator).each(function (i) {
+            placements[i].call(getSortable.call(this));
+        });
+
+    };
+
+})();
